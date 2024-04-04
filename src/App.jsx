f3cbcc3f4b4f3debf271f4cef5662e6d86217fdf
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-04-04 12:44:57 +0500

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-xl">Welcome to React with Tailwind CSS!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
